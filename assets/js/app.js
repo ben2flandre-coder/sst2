c2da5d1717c21e8421c5892146b4a5ef1bdfd56e
DEFLANDRE BENOIT <ben2flandre@gmail.com>
2026-03-18 11:03:57 +0100

    Vendor local SST source docs and livret assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   initNavigation();
   if (typeof initUI === 'function') initUI();
+
+  document.querySelectorAll('[data-print-page]').forEach((button) => {
+    button.addEventListener('click', () => window.print());
+  });
 });
